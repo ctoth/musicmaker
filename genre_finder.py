7e3c6d05cca1cd12552bf0e05f260ddb47777d1d
Tyler Spivey <tspivey@pcdesk.net>
2014-11-13 11:30:58 -0800

    Fix genre_finder
    Fix quotes in genre_finder, because %s should do it automaticly.

--- genre_finder.py
+++ genre_finder.py
@@ -8,7 +8,7 @@ def find_song_to_work_on():
 
 def update_song_info(song):
 	info = song_info.get_song_info(song['artist'], song['title'])
-	engine.execute("update lyrics set genre='%s' where artist='%s' and title='%s';", (info['genre'], song['artist'], song['title']))
+	engine.execute("update lyrics set genre=%s where artist=%s and title=%s;", (info['genre'], song['artist'], song['title']))
 	engine.execute('commit')
 	print "Set genre for %s - %s to %s" % (song['artist'], song['title'], info['genre'])
 
