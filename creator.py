15bb1592a1bffcf7eeb59ea89838410b40cfaeaf
Q <q@q-continuum.net>
2014-11-12 23:07:25 -0700

    Add lots of printing including the pid of the process, every 1000 items loaded in, etc.

--- creator.py
+++ creator.py
@@ -3,7 +3,9 @@ import loader
 
 def create_model(genre=None, lookback=3):
 	markov = MarkovChain(lookback=lookback)
-	for song in loader.songs_to_load(genre=genre):
+	for num, song in enumerate(loader.songs_to_load(genre=genre)):
+		if not num % 1000:
+			print num
 		for line in song['lyrics']:
 			markov.feed(line)
 	markov.finalize()
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
+import os
+print os.getpid()
 import creator
 import song
 import time
 
 t1 = time.time()
 s = song.Song()
+print "Creating model..."
 model = creator.create_model(lookback=3, genre='country')
 t2 = time.time()
 print "duration:", t2-t1
